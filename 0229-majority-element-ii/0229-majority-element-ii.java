fcf282300fd2276a0e26b019e13d825681ac77d0
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-09-16 13:02:45 +0530

    Time: 11 ms (43.10%), Space: 46.7 MB (52.60%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,21 @@
-class Solution {
+public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        List<Integer> result=new ArrayList<>();
-        
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        Map<Integer, Integer> map = new HashMap<>(); // Create a HashMap to store element frequencies.
+        List<Integer> result = new ArrayList<>(); // Create a list to store the majority elements.
+
+        // Iterate through the input array and count the frequencies of each element.
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        
-        int n=nums.length/3;
-        
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()>n){
-                result.add(entry.getKey());
+
+        int n = nums.length / 3; // Calculate the threshold for majority elements.
+
+        // Iterate through the HashMap entries to find majority elements.
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) { // Check if the frequency is greater than ⌊n / 3⌋.
+                result.add(entry.getKey()); // Add the majority element to the result list.
             }
         }
-        return result;
+        return result; // Return the list of majority elements.
     }
 }
