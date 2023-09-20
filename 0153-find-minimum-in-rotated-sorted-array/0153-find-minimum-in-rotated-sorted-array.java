c36fe9a8f5b4c98dcb0dd6f961a03ef9b50ee143
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-09-20 11:04:53 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (35.90%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,25 +1,28 @@
 class Solution {
     public int findMin(int[] nums) {
-        int ans=Integer.MAX_VALUE;
-        int low=0;
-        int high=nums.length-1;
-        
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[low]<=nums[high]){
-                ans=Math.min(ans,nums[low]);
-                break;
+        int ans = Integer.MAX_VALUE; // Initialize the answer as positive infinity.
+        int low = 0; // Initialize the low index.
+        int high = nums.length - 1; // Initialize the high index.
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // Calculate the middle index.
+
+            // Check if the array segment from low to high is already sorted.
+            if (nums[low] <= nums[high]) {
+                ans = Math.min(ans, nums[low]); // Update the answer with the minimum element.
+                break; // Since the array is sorted, we can exit the loop.
             }
-            
-            if(nums[low]<=nums[mid]){
-                ans=Math.min(ans,nums[low]);
-                low=mid+1;
-            }
-            else {
-                ans=Math.min(ans,nums[mid]);
-                high=mid-1;
+
+            // Check if the left half of the array (low to mid) is sorted.
+            if (nums[low] <= nums[mid]) {
+                ans = Math.min(ans, nums[low]); // Update the answer with the minimum element in the left half.
+                low = mid + 1; // Move the low index to the right of mid.
+            } else {
+                ans = Math.min(ans, nums[mid]); // Update the answer with the minimum element in the right half.
+                high = mid - 1; // Move the high index to the left of mid.
             }
         }
-        return ans;
+
+        return ans; // Return the minimum element.
     }
 }
