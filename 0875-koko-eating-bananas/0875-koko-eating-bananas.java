b0289a811b832b7edfe4bf6b0680cd70cb147126
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-09-23 12:44:09 +0530

    Time: 27 ms (33.50%), Space: 44.1 MB (69.30%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,32 +1,47 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int max=Arrays.stream(piles).max().getAsInt();
+        // Find the maximum pile size to set the upper bound for binary search.
+        int max = Arrays.stream(piles).max().getAsInt();
         
-        int low=1;
-        int high=max;
+        // Initialize low and high for binary search.
+        int low = 1;
+        int high = max;
         
-        while(low<=high){
-            int mid=(low+high)/2;
-            int totalTime=findTotalTime(piles,mid);
-            if(totalTime<=h) {
-                high=mid-1;
-            }
-            else {
-                low=mid+1;
+        // Perform binary search.
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            // Calculate the total time required to eat all piles at the current speed.
+            int totalTime = findTotalTime(piles, mid);
+            
+            // Adjust the search space based on the total time.
+            if (totalTime <= h) {
+                // If the total time is within the allowed hours, reduce the upper bound.
+                high = mid - 1;
+            } else {
+                // If the total time exceeds the allowed hours, increase the lower bound.
+                low = mid + 1;
             }
         }
+        
+        // The lowest speed at which it's possible to eat all piles in 'h' hours.
         return low;
     }
     
-    public int findTotalTime(int[] piles,int k){
-        int time=0;
+    // Function to calculate the total time required to eat all piles at a given speed 'k'.
+    public int findTotalTime(int[] piles, int k) {
+        int time = 0;
         
-        for(int i=0;i<piles.length;i++){
-            time+= Math.ceil((double)piles[i]/k);
+        for (int i = 0; i < piles.length; i++) {
+            // Calculate the time required to eat the current pile at speed 'k' and round up to the nearest integer.
+            time += Math.ceil((double) piles[i] / k);
         }
+        
         return time;
     }
-    
+}
+
+//function for brute force approach
     //     public int minEatingSpeed(int[] piles, int h) {
 //         int max=Arrays.stream(piles).max().getAsInt();
 //         for(int i=1;i<=max;i++){
@@ -37,4 +52,3 @@ class Solution {
 //         }
 //         return max;
 //     }
-}
