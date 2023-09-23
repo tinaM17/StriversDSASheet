276fdbc7d5d381910a04f9017dca6734df7e0bbe
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-09-23 12:03:22 +0530

    Time: 1 ms (89.70%), Space: 39.8 MB (99.29%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,25 +1,41 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int lengthOfT=t.length();
-        int lengthOfS=s.length();
-        StringBuilder string=new StringBuilder();
-        int i=0;
-        int j=0;
-        while(i<lengthOfT && j<lengthOfS){
-            char ch1=t.charAt(i);
-            char ch2=s.charAt(j);
-            if(ch1==ch2){
+        // Calculate the lengths of the input strings.
+        int lengthOfT = t.length();
+        int lengthOfS = s.length();
+        
+        // Create a StringBuilder to store matching characters from t.
+        StringBuilder string = new StringBuilder();
+        
+        // Initialize indices for iterating through t and s.
+        int i = 0;
+        int j = 0;
+        
+        // Iterate through t and s.
+        while (i < lengthOfT && j < lengthOfS) {
+            // Extract characters at the current positions.
+            char ch1 = t.charAt(i);
+            char ch2 = s.charAt(j);
+            
+            // Compare characters.
+            if (ch1 == ch2) {
+                // Append matching character to the result.
                 string.append(ch1);
+                
+                // Move to the next characters in both strings.
                 i++;
                 j++;
-            }
-            else {
+            } else {
+                // If characters don't match, only increment i to search for the next character in t.
                 i++;
             }
         }
-        if(string.toString().equals(s)){
-            return true;
+        
+        // Check if the result string is equal to s.
+        if (string.toString().equals(s)) {
+            return true; // s is a subsequence of t.
         }
-        return false;
+        
+        return false; // s is not a subsequence of t.
     }
 }
