3113ec63c1db8fe64e7fc219f46965a80891eebb
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-11-21 11:09:06 +0530

    Time: 0 ms (100.00%), Space: 56.2 MB (12.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,13 +1,35 @@
 class Solution {
     public void rotate(int[] nums, int k) {
     
-        int length=nums.length;
-        int temp[]=new int[length];
-        for(int i=0;i<length;i++){
-            temp[(i+k)%length]=nums[i];
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=temp[i];
+        //This approach is using an temp extra space array
+        // int length=nums.length;
+        // int temp[]=new int[length];
+        // for(int i=0;i<length;i++){
+        //     temp[(i+k)%length]=nums[i];
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     nums[i]=temp[i];
+        // }
+        
+        // Optimised approach without using extra space
+        
+        k=k%nums.length;
+        int n=nums.length;
+        
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+        
+        
+    }
+    
+    private void reverse(int[] nums,int start,int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
         }
     }
 }
