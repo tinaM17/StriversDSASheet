5cdb624ccbd1811e179cdc16aa6db48acc993fb6
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-11-21 10:40:46 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (81.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,39 +1,65 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i=0;
-        int j=0;
+        //Three Pointers Approach or Dutch National Flag Algorithm
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
         
-        while(j<nums.length){
-            if(nums[j]==0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-                j++;
+        while(mid<=high){
+            if(nums[mid]>1){
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+            else if(nums[mid]<1){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
             }
             else {
-                j++;
+                mid++;
             }
         }
         
-        i=0;
-        j=0;
         
-        while(j<nums.length){
-            if(nums[j]==0 && nums[i]==0){
-                i++;
-                j++;
-            }
-            else if(nums[j]==1){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-                j++;
-            }
-            else {
-                j++;
-            }
-        }
+        //Using two pointer approach.
+//         int i=0;
+//         int j=0;
+        
+//         while(j<nums.length){
+//             if(nums[j]==0){
+//                 int temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 i++;
+//                 j++;
+//             }
+//             else {
+//                 j++;
+//             }
+//         }
+        
+//         i=0;
+//         j=0;
+        
+//         while(j<nums.length){
+//             if(nums[j]==0 && nums[i]==0){
+//                 i++;
+//                 j++;
+//             }
+//             else if(nums[j]==1){
+//                 int temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 i++;
+//                 j++;
+//             }
+//             else {
+//                 j++;
+//             }
+//         }
     }
 }
