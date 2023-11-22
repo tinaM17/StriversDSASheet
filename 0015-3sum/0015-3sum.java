9ab6f7861bdb78ab9a68251c8e29f7a00123834d
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-11-22 11:17:58 +0530

    Time: 461 ms (12.47%), Space: 50.9 MB (45.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>(); // Initialize a list to store the result triplets.
-        int n = nums.length;
-        Arrays.sort(nums); // Sort the input array in ascending order.
-
-        for (int i = 0; i < n; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
-                // Skip duplicate elements to avoid duplicate triplets.
-                continue;
-            }
-            int j = i + 1; // Pointer to the element after nums[i].
-            int k = n - 1; // Pointer to the last element in the array.
-
+         int target = 0;
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++; // If the sum is less than zero, move the left pointer to the right.
-                } else if (sum > 0) {
-                    k--; // If the sum is greater than zero, move the right pointer to the left.
-                } else {
-                    // If the sum is zero, we found a triplet.
-                    List<Integer> triplets = Arrays.asList(nums[i], nums[j], nums[k]);
-                    ans.add(triplets); // Add the triplet to the result list.
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-
-                    // Skip any duplicate elements to avoid duplicate triplets.
-                    while (j < k && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k + 1]) {
-                        k--;
-                    }
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-        return ans; // Return the list of unique triplets that sum to zero.
+        output.addAll(s);
+        return output;
     }
 }
