43ce33f7c230d74f66b11711a4bc13d8d2fd56bc
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-11-23 13:29:17 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (7.81%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,18 +11,41 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set=new HashSet<>();
         
-        ListNode prev=null;
-        ListNode curr=head;
+        ListNode meet=isCyclePresent(head);
+        if(meet==null) return null;
+        ListNode start=head;
         
-        while(curr!=null){
-            if(set.contains(curr)){
-                return curr;
-            }
-            set.add(curr);
-            prev=curr;
-            curr=curr.next;
+        while(start!=meet){
+            start=start.next;
+            meet=meet.next;
+        }
+        return start;
+//         Set<ListNode> set=new HashSet<>();
+        
+//         ListNode prev=null;
+//         ListNode curr=head;
+        
+//         while(curr!=null){
+//             if(set.contains(curr)){
+//                 return curr;
+//             }
+//             set.add(curr);
+//             prev=curr;
+//             curr=curr.next;
+//         }
+//         return null;
+    }
+    
+    public ListNode isCyclePresent(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+                return slow;
         }
         return null;
     }
