87eedb9ac5b8db13ff42d9e1382c28d0087feaae
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-11-24 13:05:05 +0530

    Time: 10 ms (95.35%), Space: 47.9 MB (12.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,48 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+       HashMap<Integer,Integer> map=new HashMap<>();
         
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+         
+        List<Integer> bucket[] = new ArrayList[nums.length + 1];
         
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)-> map.get(b)-map.get(a));
-        for(int key: map.keySet()){
-            pq.add(key);
-        }
-        int[] res=new int[k];
-        for(int i=0;i<k;i++){
-            res[i]=pq.poll();
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            if(bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
         }
         
+        int res[] = new int[k];
+        int index = 0;
+        for(int i = bucket.length - 1; i >= 0; i--){
+            if(bucket[i] != null){
+                for(int val : bucket[i]){
+                    res[index++] = val;
+                    if(index == k) return res;
+                }
+            }
+        }
         return res;
+      
+//         HashMap<Integer,Integer> map=new HashMap<>();
+        
+//         for(int i=0;i<nums.length;i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//         }
+        
+//         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)-> map.get(b)-map.get(a));
+//         for(int key: map.keySet()){
+//             pq.add(key);
+//         }
+//         int[] res=new int[k];
+//         for(int i=0;i<k;i++){
+//             res[i]=pq.poll();
+//         }
+        
+//         return res;
     }
 }
