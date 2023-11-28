e6654b22244c0aed3dcfb9c848ce9ad2697095d6
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-11-28 11:29:50 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (5.19%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,40 +1,35 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int low = 0; // Initialize the low index.
-        int high = nums.length - 1; // Initialize the high index.
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2; // Calculate the middle index.
-
-            // Check if the middle element equals the target.
-            if (target == nums[mid]) {
-                return true; // Target found, return true.
+        int low=0;
+        int high=nums.length-1;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target) return true;
+            
+            else if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                mid++;
+                continue;
             }
-
-            // Check if there are duplicates at the low, middle, and high positions.
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++; // Move the low index to skip the duplicate.
-                mid++; // Move the middle index to maintain consistency.
-                continue; // Continue to the next iteration.
-            }
-
-            // Check if the left half (low to mid) is sorted.
-            if (nums[low] <= nums[mid]) {
-                if (target < nums[mid] && target >= nums[low]) {
-                    high = mid - 1; // Target is in the left half.
-                } else {
-                    low = mid + 1; // Target is in the right half.
+            else if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
                 }
-            } else {
-                // The right half (mid to high) is sorted.
-                if (target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1; // Target is in the right half.
-                } else {
-                    high = mid - 1; // Target is in the left half.
+                else {
+                    low=mid+1;
+                }
+            }
+            else {
+                if(target<=nums[high] && target>nums[mid]){
+                    low=mid+1;
+                }
+                else {
+                    high=mid-1;
                 }
             }
         }
-
-        return false; // Target not found, return false.
+        return false;
     }
 }
