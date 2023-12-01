13491301f8359fe89837683a29d752757285e3f9
Tina Majumder <97033681+tinaM17@users.noreply.github.com>
2023-12-01 12:15:48 +0530

    Time: 5 ms (57.11%), Space: 62 MB (18.13%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,26 +1,16 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int sum=Arrays.stream(nums).sum();
-        int n=nums.length;
-        int target=sum-x;
-        if(target<0)
-            return -1;
-        if(target==0)
-            return n;
-        
-        int count=Integer.MAX_VALUE;
-        int left=0, right=0,currSum=0;
-        
-        while(right<n){
-            currSum+=nums[right];
-            right++;
-            while(currSum>target && left<n){
-                currSum-=nums[left];
-                left++;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        int maxLength = -1, currSum = 0;
+        // Here l -> left r -> right
+        for (int l=0, r=0; r<nums.length; r++) {
+            currSum += nums[r];
+            while (l <= r && currSum > sum - x) currSum -= nums[l++];
+            if (currSum == sum - x) {
+                maxLength = Math.max(maxLength, r-l+1);
             }
-            if(currSum==target)
-                count=Math.min(count,n-(right-left));
         }
-        return (count==Integer.MAX_VALUE) ?-1 : count;
+        return maxLength == -1 ? -1 : nums.length - maxLength;
     }
 }
